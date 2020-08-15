6e1d7e93fdf1eed4eb03f722055cd18689f6fe9d
Bruno Amaral <brunoamaralf@gmail.com>
2020-08-15 08:58:08 -0300

    Update get_pods.go target namespace

--- get_pods.go
+++ get_pods.go
@@ -22,7 +22,7 @@ func getPods() (map[string]string) {
    // Pods in all namespaces
    var pods corev1.PodList
    
-   if err = client.List(context.Background(), "kube-system", &pods); err != nil {
+   if err = client.List(context.Background(), "monitoring", &pods); err != nil {
 	   log.Fatal(err)
    }
       
