07c88e174ee82f06f544819fbcabb688376dc303
Bruno Amaral <brunoamaralf@gmail.com>
2020-08-17 15:53:20 -0300

    Update http_handlers.go

--- http_handlers.go
+++ http_handlers.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
     "net/http"
 )
 
 
 func healthCheck(w http.ResponseWriter, req *http.Request) { 
-	output,_ := json.Marshal( "200: {Status: OK}" )
-    fmt.Println(w, output)
+    fmt.Fprintf(w, "200: {Status: OK}")
 }
